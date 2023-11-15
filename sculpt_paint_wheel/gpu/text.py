579a67828d40abca95c40d7a2cbf48ab8fd84f20
jfranmatheu <45881831+jfranmatheu@users.noreply.github.com>
2023-11-15 22:16:31 +0100

    4.0 support: blf.size doesn't support dpi param

--- sculpt_paint_wheel/gpu/text.py
+++ sculpt_paint_wheel/gpu/text.py
@@ -30,7 +30,7 @@ def Draw_Text(_x, _y, _text, _size, _font_id = 0, _r = 1, _g = 1, _b = 1, _a = 1
         text_shadow_offset(_font_id, -1, -1)
     text_color(_font_id, _r, _g, _b, _a)
     text_position(_font_id, _x, _y, 0) # -6/-6 para "o" #
-    text_size(_font_id, _size, 72)
+    text_size(_font_id, _size)
     text_draw(_font_id, _text)
     if _use_shadow:
         text_disable(_font_id, SHADOW)
@@ -43,7 +43,7 @@ def Draw_Text_Right(_x, _y, _text, _size, _font_id = 0, _r = 1, _g = 1, _b = 1, 
         text_enable(_font_id, SHADOW)
         text_shadow(_font_id, 3, 0, 0, 0, .6)
         text_shadow_offset(_font_id, -1, -1)
-    text_size(_font_id, _size, 72)
+    text_size(_font_id, _size)
     dim = text_dim(_font_id, _text)
     text_color(_font_id, _r, _g, _b, _a)
     text_position(_font_id, _x-dim[0], _y - dim[1]*.5, 0) # -6/-6 para "o" #
